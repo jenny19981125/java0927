7834c2876f9bea4d109caa384e6273662751e6c8
jenny19981125 <jenny19981125@gmail.com>
2017-09-27 20:07:12 +0800

    aaa

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.print("106024260 邱郁雯");
-        System.out.print("106024260");// write your code here
+        System.out.print("106024260");
+        System.out.print("1A");// write your code here
     }
 }
